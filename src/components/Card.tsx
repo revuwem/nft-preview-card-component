e6ad3189d4eb3d25c5801412ab1458fb2b4d7a7b
Karina Grishaeva <revuwem@gmail.com>
2023-01-05 00:15:54 +0400

    feat(Card): add text font-size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,12 +4,14 @@ const Card: React.FC<ICardProps> = () => {
   return (
     <section>
       <img src="/images/image-equilibrium.jpg" alt="Image of equilibrium" />
-      <h3>Equilibrium #3429</h3>
-      <p>Our Equilibrium collection promotes balance and calm.</p>
-      <p>0.041 ETH</p>
-      <p>3 days left</p>
+      <h3 className="text-2xl font-semibold">Equilibrium #3429</h3>
+      <p className="text-lg font-light">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <p className="text-lg uppercase">0.041 ETH</p>
+      <p className="text-lg">3 days left</p>
       <img src="/images/image-avatar.png" alt="Image of author" />
-      <p>Creation of Jules Wyvern</p>
+      <p className="text-lg">Creation of Jules Wyvern</p>
     </section>
   );
 };
