835c41f90934cd4db82c96bf52c0869024cbbdfd
Karina Grishaeva <revuwem@gmail.com>
2023-01-05 01:00:35 +0400

    feat(Card): style content alignment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,16 +2,29 @@ interface ICardProps {}
 
 const Card: React.FC<ICardProps> = () => {
   return (
-    <section>
-      <img src="/images/image-equilibrium.jpg" alt="Image of equilibrium" />
-      <h3 className="text-2xl font-semibold">Equilibrium #3429</h3>
-      <p className="text-lg font-light">
+    <section className="p-5 bg-blue-200 rounded-xl">
+      <img
+        src="/images/image-equilibrium.jpg"
+        alt="Image of equilibrium"
+        className="rounded-lg mb-5"
+      />
+      <h3 className="text-xl font-semibold mb-3">Equilibrium #3429</h3>
+      <p className="text-base font-light mb-3">
         Our Equilibrium collection promotes balance and calm.
       </p>
-      <p className="text-lg uppercase">0.041 ETH</p>
-      <p className="text-lg">3 days left</p>
-      <img src="/images/image-avatar.png" alt="Image of author" />
-      <p className="text-lg">Creation of Jules Wyvern</p>
+      <div className="flex justify-between">
+        <p className="text-base uppercase">0.041 ETH</p>
+        <p className="text-base">3 days left</p>
+      </div>
+      <hr className="border-blue-300 my-3" />
+      <div className="flex items-center gap-4">
+        <img
+          src="/images/image-avatar.png"
+          alt="Image of author"
+          className="max-w-[2rem] rounded-full border-white border-2"
+        />
+        <p className="text-sm">Creation of Jules Wyvern</p>
+      </div>
     </section>
   );
 };
