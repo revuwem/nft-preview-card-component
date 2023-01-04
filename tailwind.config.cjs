9abf7181c9c1694d876672abc74bb0316377d673
Karina Grishaeva <revuwem@gmail.com>
2023-01-05 00:07:38 +0400

    feat(tailwind): extend color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        cyan: "hsl(178, 100%, 50%)",
+        blue: {
+          100: "hsl(217, 54%, 11%)",
+          200: "hsl(216, 50%, 16%)",
+          300: "hsl(215, 32%, 27%)",
+          400: "hsl(215, 51%, 70%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
