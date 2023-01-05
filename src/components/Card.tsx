d9cd470b4e24ccb328f6a4b6848126f9bd04b5c9
Karina Grishaeva <revuwem@gmail.com>
2023-01-05 14:43:28 +0400

    fix(Card): desktop layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,13 @@ interface ICardProps {}
 
 const Card: React.FC<ICardProps> = () => {
   return (
-    <section className="max-w-sm p-5 bg-blue-200 rounded-xl">
-      <div className="relative overflow-hidden rounded-lg mb-4 h-2/5">
-        <img src="images/image-equilibrium.jpg" alt="Image of equilibrium" />
+    <section className="max-w-sm p-5 pb-7 bg-blue-200 rounded-xl">
+      <div className="relative overflow-hidden rounded-lg mb-4">
+        <img
+          src="images/image-equilibrium.jpg"
+          alt="Image of equilibrium"
+          className="max-h-[20rem] w-full object-cover"
+        />
         <a
           href="#"
           className="opacity-0 hover:opacity-100 bg-cyan/[.6] w-full h-full absolute inset-0 grid place-items-center transition"
@@ -12,7 +16,7 @@ const Card: React.FC<ICardProps> = () => {
           <img src="images/icon-view.svg" alt="View" />
         </a>
       </div>
-      <h3 className="text-base lg:text-lg font-semibold mb-3">
+      <h3 className="text-base lg:text-xl font-semibold mb-3">
         <a
           href="#"
           target="_blank"
